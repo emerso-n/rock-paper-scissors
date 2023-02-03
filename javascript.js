9252d42b5519d0ad6a54992d1353b7a219cfea20
emerso-n <emerson.skotz@gmail.com>
2023-02-03 01:26:31 -0500

    done

--- javascript.js
+++ javascript.js
@@ -1,14 +1,18 @@
 console.info("Javascript loaded")
-//you could also make variables for Rock, Paper, and Scissors strings to keep them consistent. idk maybe do that later? yeah seems worth idk
+
 const rock = "rock"
 const paper = "paper"
 const scissors = "scissors"
 
 const Outcomes = { win: "YOU WIN", loss: "YOU LOSE", tie: "TIE" }
 
+let playerScore = 0
+let computerScore = 0
+
 const playerChoiceDiv = document.querySelector("#player-choice")
 const computerChoiceDiv = document.querySelector("#computer-choice")
 const resultsTextDiv = document.querySelector("#result-text")
+const scoreDiv = document.querySelector("#score")
 
 const choiceView = document.querySelector('#buttons_con')
 const resultsView = document.querySelector('#results_con')
@@ -16,19 +20,28 @@ const resultsView = document.querySelector('#results_con')
 const choiceBtns = choiceView.querySelectorAll("button")
 const againBtn = document.querySelector("#results_div button")
 
-choiceBtns.forEach(button => button.addEventListener('click', displayResult))
+choiceBtns.forEach(button => { button.addEventListener('click', handAnimate); button.addEventListener('animationend', displayResult) })
 
 againBtn.addEventListener('click', switchView);
 
+function handAnimate(e) {
+    e.target.classList.add("hand-animate-out")
+}
+
 function displayResult(e) {
+    e.target.classList.remove("hand-animate-out")
+
     let playerChoice = e.target.id;
     let computerChoice = getComputerChoice();
     let outcome = getOutcome(playerChoice, computerChoice)
 
     playerChoiceDiv.style.backgroundImage = `url(static/images/hand_${playerChoice}.png)`;
+
     computerChoiceDiv.style.backgroundImage = `url(static/images/hand_${computerChoice}.png)`;
+
     resultsTextDiv.textContent = outcome;
-    switchView()
+    scoreDiv.textContent = `${playerScore} - ${computerScore}`;
+    switchView();
 }
 
 const rpsArray = [rock, paper, scissors]
@@ -42,9 +55,11 @@ function getOutcome(playerSelection, computerSelection) {
         return Outcomes.tie
     }
     else if (playerSelection == rock && computerSelection == scissors || playerSelection == paper && computerSelection == rock || playerSelection == scissors && computerSelection == paper) {
+        ++playerScore
         return Outcomes.win
     }
     else {
+        ++computerScore
         return Outcomes.loss
     }
 }
